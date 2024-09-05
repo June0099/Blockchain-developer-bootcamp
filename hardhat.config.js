9c80da34b2d11422bb38e225b469bc3ebc4d477b
june <chiboywin33@gmail.com>
2024-09-05 16:07:10 -0700

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+task("accounts", "Prints the lists of accounts" , async (taskArgs, hre ) => {
+  const accounts = await hre.ethers.getSigners();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+ /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     localhost:{}
-  }
+  },
 };
