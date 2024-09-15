dc58728fffc0505df503f93fc0b139c957c77318
june <chiboywin33@gmail.com>
2024-09-15 10:05:54 -0700

    22. Withdrawals

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -53,7 +53,11 @@ export const loadExchange = async (provider, address, dispatch) => {
 
 export const subscribeToEvents = (exchange, dispatch) => {
 	exchange.on('Deposit', (token, user, amount, balance, event) => {
-		dispatch({ type: 'TRANSFER_SUCCESS', event })
+	  dispatch({ type: 'TRANSFER_SUCCESS', event })
+	})
+
+   exchange.on('Withdraw', (token, user, amount, balance, event) => {
+	  dispatch({ type: 'TRANSFER_SUCCESS', event })
 	})
 }
 
@@ -80,19 +84,23 @@ export const loadBalances = async (exchange, tokens, account, dispatch) => {
 // TRANSFER TOKENS (DEPOSIT & WITHDRAWS)
 
 export const transferTokens = async (provider, exchange, transferType, token, amount, dispatch) => {
-	let transaction
+  let transaction
 
-	dispatch({ type: 'TRANSFER_REQUEST'})
+  dispatch({ type: 'TRANSFER_REQUEST'})
 
 	try {
-   const signer = await provider.getSigner()
-	const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18)
+     const signer = await provider.getSigner()
+     const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18)
 
-	transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
-	await transaction.wait()
-	transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
-	await transaction.wait()
+	  if(transferType === 'Deposit') {
+	    transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
+	    await transaction.wait()
+	    transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+	   } else {
+	    transaction = await exchange.connect(signer).withdrawToken(token.address, amountToTransfer)
+	   }
 
+	   await transaction.wait()
 
 	} catch(error) {
 	  dispatch({ type: 'TRANSFER_FAIL'})
