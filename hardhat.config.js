5d611e1c1e5dfa9ca7b5ea28d24ed6ec609682a6
june <chiboywin33@gmail.com>
2024-09-20 09:26:47 -0700

    sepolia setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-
-task("accounts", "Prints the lists of accounts" , async (taskArgs, hre ) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
  /** 
-  * @type import('hardhat/config').HardhatUserConfig */
+  * @type import('hardhat/config').HardhatUserConfig 
+  */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost:{}
+    localhost:{},
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(",")
+    }
   },
 };
